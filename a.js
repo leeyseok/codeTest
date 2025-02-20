e489700c8438f839a397516bb971b9edb92fb5f7
leeyseok <88834341+leeyseok@users.noreply.github.com>
2025-02-20 22:34:51 +0900

    day4

--- a.js
+++ a.js
@@ -1,47 +1,26 @@
-function solution(park, routes) {
-    const direction = {
-        N: [-1, 0],
-        E: [0, 1],
-        S: [1, 0],
-        W: [0, -1]
-    };
-
-    const numRows = park.length
-    const numCols = park[0].length
-    let currentRow = 0, currentCol = 0;
-
-    for (let i = 0; i < numRows; i++) {
-        for (let j = 0; j < numCols; j++) {
-            if (park[i][j] === 'S') {
-                currentRow = i;
-                currentCol = j;
-            }
-        }
-    }
-    routes.forEach(order => {
-        const [directionKey, steps] = order.split(' ');
-        const [dx, dy] = direction[directionKey];
-        let row = currentRow, col = currentCol;
-        let canMove = true;
-        for (let i = 0; i < parseInt(steps); i++) {
-            const newRow = row + dx;
-            const newCol = col + dy;
-            if (!(0 <= newRow && newRow < numRows && 0 <= newCol && newCol < numCols && park[newRow][newCol] !== 'X')) {
-                canMove = false;
-                break;
-            }
-
-            row = newRow;
-            col = newCol;
-        }
-        if (canMove) {
-            currentRow = row;
-            currentCol = col;
+function solution(today, terms, privacies) {
+    const endDay = 28
+    var answer = [];
+    const [ year, month, day ] = today.split('.').map(Number)
+    const todayDays = (year * 12 * 28) + (28 * month) + day
+    const termsInfo = {}
+    terms.forEach((term) => {
+        const [key, value] = term.split(' ')
+        termsInfo[key] = value
+    })
+    privacies.forEach((privacy, index) => {
+        const [startDay, type] = privacy.split(' ')
+        const [sy, sm, sd] = startDay.split('.').map(Number)
+        const expiredDate = (sy * 12 * 28) + (28 * sm) + sd + (28 * termsInfo[type]) - 1
+        if (expiredDate < todayDays) {
+            console.log(todayDays)
+            console.log(expiredDate)
+            answer.push(index + 1);
         }
     });
 
-    return [currentRow, currentCol];
+    console.log(answer)
+    return answer;
 }
 
-console.log(solution(["SOO", "OOO", "OOO"], ["E 2", "S 2", "W 1"]));
-console.log(solution(["SOO", "OXX", "OOO"], ["E 2", "S 2", "W 1"]));
+solution("2022.05.19",["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])
