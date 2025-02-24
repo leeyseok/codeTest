8c58c3cb515d801b59a9fe47b8a03a0cd3d77144
leeyseok <88834341+leeyseok@users.noreply.github.com>
2025-02-24 18:40:33 +0900

    재귀적별찍기 가운데는 공백패턴대체

--- level2/재귀적별찍기.js
+++ level2/재귀적별찍기.js
@@ -2,16 +2,14 @@ function solution(n) {
     if (n === 1) return "*";
 
     let prev = solution(n / 3).split("\n")
-    console.log(prev)
     let length = prev.length;
     let result = []
+
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < length; j++) {
             if (i === 1) {
-                // 중간에 공백 그전까진 기존
                 result.push(prev[j] + " ".repeat(length) + prev[j]); 
             } else {
-                // 나머지 기존반복
                 result.push(prev[j].repeat(3))
             }
         }
