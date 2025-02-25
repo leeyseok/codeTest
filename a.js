b1a12cb1fee8ae216398f96faf6c700604d13f6f
leeyseok <88834341+leeyseok@users.noreply.github.com>
2025-02-25 21:54:26 +0900

    코테대회문제

--- a.js
+++ a.js
@@ -1,25 +1,48 @@
-function solution(n) {
-    if (n === 1) return "*";
+function solution(n, w, num) {
+    let boxes = [];
+    let f = 0;
+    let d = 'r';
+    for (let i = 1; i <= n; i++) {
+        if (!boxes[f]) boxes[f] = [];
 
-    let prev = solution(n / 3).split("\n")
-    console.log(prev)
-    let length = prev.length;
-    let result = []
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < length; j++) {
-            if (i === 1) {
-                // 중간에 공백 그전까진 기존
-                result.push(prev[j] + " ".repeat(length) + prev[j]); 
-            } else {
-                // 나머지 기존반복
-                result.push(prev[j].repeat(3))
-            }
+        if (d === 'r') {
+            boxes[f].push(i);
+        } else {
+            boxes[f].unshift(i);
+        }
+
+        if (i % w === 0) {
+            d = d === 'r' ? 'l' : 'r';
+            f++;
         }
     }
-    return result.join("\n")
+    console.log('boxes', boxes.length);
+    if ( boxes[boxes.length- 1].length < w) {
+        for ( let j = boxes[boxes.length- 1].length; j < w; j++) {
+            if (d === 'l' ) {
+                boxes[boxes.length- 1].unshift(0);
+            } else {
+                boxes[boxes.length- 1].push(0);
+
+            } 
+        }
+    }
+    console.log('boxes', boxes);
+    const tf = boxes.findIndex(box => box.includes(num));
+    console.log('tf', tf);
+    const idx = boxes[tf].indexOf(num);
+    console.log('idx', idx);
+    // const maxH = boxes.length;
+    const maxH = (boxes[boxes.length - 1][idx]) !== 0 ? boxes.length : boxes.length - 1;
+    console.log('boxes[boxes.length - 1][idx]', boxes[boxes.length - 1][idx]);
+    console.log('boxes.length', boxes.length);
+    console.log('maxH', maxH);
+    console.log( maxH - tf);
+    return maxH - tf;
 }
-console.log(solution(3))
-console.log('-----------')
-console.log(solution(9))
-console.log('-----------')
-console.log(solution(27))
+
+// Example usage:
+let n = 22;
+let w = 6;
+// solution(	22, 6, 8);
+solution(13, 3, 10);
